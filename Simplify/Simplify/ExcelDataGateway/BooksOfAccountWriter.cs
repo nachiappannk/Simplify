6ec636c12659a38b06f64055626ee6718da0f83b
Nachiappan <nachiappan.nk@siemens.com>
2017-05-06 14:44:37 +0530

    Added formating to the output excel

--- Simplify/Simplify/ExcelDataGateway/BooksOfAccountWriter.cs
+++ Simplify/Simplify/ExcelDataGateway/BooksOfAccountWriter.cs
@@ -4,191 +4,213 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using OfficeOpenXml;
+using OfficeOpenXml.Style;
 using Simplify.Books;
 using Simplify.DataGateway;
+using System.Drawing;
 
 namespace Simplify.ExcelDataGateway
 {
+    public class ExcelWriter : IDisposable
+    {
+        private ExcelPackage _package;
+        private ExcelWorksheet _workSheet;
+        public ExcelWriter(string fileName, string sheetName)
+        {
+            FileInfo file = new FileInfo(fileName);
+            _package = new ExcelPackage(file);
+            _workSheet = _package.Workbook.Worksheets.Add(sheetName);
+
+        }
+
+        public void Write(int zeroBasedRowIndex, params object[] values)
+        {
+            for (int j = 0; j < values.Length; j++)
+            {
+                int column = j + 1;
+                int row = zeroBasedRowIndex + 1;
+                _workSheet.Cells[row, column].Value = values[j];
+                if (values[j] is double)
+                    _workSheet.Cells[row, column].Style.Numberformat.Format = "#,##0.00";
+                else if (values[j] is DateTime)
+                    _workSheet.Cells[row, column].Style.Numberformat.Format = "dd-mmm-yy";
+            }
+        }
+
+        public void WriteList<T>(int zeroBasedStartingRowIndex, IList<T> items,
+                Func<T ,int, object[]> itemInterpretor)
+        {
+            int row = zeroBasedStartingRowIndex;
+            foreach (var item in items)
+            {
+                Write(row++, itemInterpretor.Invoke(item, row));
+            }
+        }
+
+        public void SetColumnsWidth(params double[] widths)
+        {
+            for (int i = 1; i <= widths.Length; i++)
+            {
+                _workSheet.Column(i).Width = widths[i - 1];
+            }
+        }
+
+        public void ApplyHeadingFormat(int numberOfColumns)
+        {
+            using (var range = _workSheet.Cells[1, 1, 1, numberOfColumns])
+            {
+                range.Style.Font.Bold = true;
+                range.Style.Fill.PatternType = ExcelFillStyle.Solid;
+                range.Style.Fill.BackgroundColor.SetColor(Color.DarkBlue);
+                range.Style.Font.Color.SetColor(Color.White);
+                range.AutoFilter = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            _package.Save();
+            _package.Dispose();
+        }
+    }
+
+
+
     public class BooksOfAccountWriter : IBooksOfAccountWriter
     {
         private string outputExcelFileName;
 
         public BooksOfAccountWriter(string outputExcelFileName)
         {
-
             this.outputExcelFileName = outputExcelFileName;
         }
 
         public void WriteBooksOfAccount(ConsolidatedBook consolidatedBook)
         {
-            FileInfo newFile = new FileInfo(outputExcelFileName);
-            if (newFile.Exists)
+            AddJournal(consolidatedBook.Journal);
+            AddTrialBalance(consolidatedBook.TrialBalance);
+            AddProfitAndLoss(consolidatedBook.ProfitAndLoss);
+            AddCapitalAccount(consolidatedBook.CapitalAccount);
+            AddBalanceSheet(consolidatedBook.BalanceSheetBook);
+        }
+
+        private void AddBalanceSheet(BalanceSheetBook balanceSheet)
+        {
+            var bSheetCopy = balanceSheet.Where(s => Math.Abs(s.Value) > 0.001).ToList();
+            var index = 0;
+            using (var writer = new ExcelWriter(outputExcelFileName, "BS"))
             {
-                newFile.Delete(); // ensures we create a new workbook
-                newFile = new FileInfo(outputExcelFileName);
-            }
-            using (ExcelPackage package = new ExcelPackage(newFile))
-            {
-                AddJournal(package, consolidatedBook.Journal);
-                AddTrialBalance(package, consolidatedBook.TrialBalance);
-                AddProfitAndLoss(package, consolidatedBook.ProfitAndLoss);
-                AddCapitalAccount(package, consolidatedBook.CapitalAccount);
-                AddBalanceSheet(package, consolidatedBook.BalanceSheetBook);
-            }
+                object[] headings = {"S.No.", "Ledger", "Credit", "Debit", "Total"};
+                writer.Write(index++,headings);
+                writer.SetColumnsWidth(6, 45, 12, 12, 12);
+                writer.ApplyHeadingFormat(headings.Length);
 
-            
-        }
-
-        private void AddBalanceSheet(ExcelPackage package, BalanceSheetBook balanceSheet1)
-        {
-            ExcelWorksheet worksheet = AddWorksheet(package, "BS");
-            var bSheetCopy = balanceSheet1.Where(s => Math.Abs(s.Value) > 0.001).ToList();
-
-
-            var index = 1;
-            AddTableHeading(worksheet, index++, "Ledger", "Credit", "Debit");
-            AddStatements(bSheetCopy, worksheet, index);
-            index = index + bSheetCopy.Count;
-            AddTableClosing(bSheetCopy, worksheet, index, "Total");
-            package.Save();
-        }
-
-
-        private void AddDate(ExcelWorksheet sheet, int row, int column, DateTime date)
-        {
-            sheet.Cells[row, column].Value = date;
-            sheet.Cells[row, column].Style.Numberformat.Format = "dd-mmm-yyyy";
-        }
-
-        private void Add2DecimalNumber(ExcelWorksheet sheet, int row, int column, double value)
-        {
-            sheet.Cells[row, column].Value = value;
-            sheet.Cells[row, column].Style.Numberformat.Format = "#,##0.00";
-        }
-
-        private void Add0DecimalNumber(ExcelWorksheet sheet, int row, int column, double value)
-        {
-            sheet.Cells[row, column].Value = value;
-        }
-
-        private void AddText(ExcelWorksheet sheet, int row, int column, string value)
-        {
-            sheet.Cells[row, column].Value = value;
-        }
-
-        private void AddCapitalAccount(ExcelPackage package, CapitalAccountBook capitalAccountBook)
-        {
-            ExcelWorksheet worksheet = AddWorksheet(package, "CapitalAccount");
-
-            int index = 1;
-
-            AddTableHeading(worksheet, index++, "Date","Ledger","Credit","Debit");
-            AddDateStatements(capitalAccountBook, worksheet, index);
-            index = index + capitalAccountBook.Count;
-            AddDatedTableClosing(capitalAccountBook, worksheet, index, "Closing Balance");
-            package.Save();
-        }
-
-        private void AddDatedTableClosing(CapitalAccountBook capitalAccountBook, ExcelWorksheet worksheet, int index,
-            string totalName)
-        {
-            AddDate(worksheet, index, 1, capitalAccountBook.ClosingDate);
-            AddText(worksheet, index, 2, totalName);
-            Add2DecimalNumber(worksheet, index, 3, capitalAccountBook.GetCreditTotal());
-            Add2DecimalNumber(worksheet, index, 4, capitalAccountBook.GetDebitTotal());
-            Add2DecimalNumber(worksheet, index, 5, capitalAccountBook.GetTotal());
-        }
-
-        private void AddDateStatements(IList<DatedStatement> datedStatements, ExcelWorksheet worksheet, int startingRow)
-        {
-            foreach (var statement in datedStatements)
-            {
-                AddDate(worksheet, startingRow, 1, statement.Date);
-                AddText(worksheet, startingRow, 2, statement.Name);
-                Add2DecimalNumber(worksheet, startingRow, 3, statement.GetCreditValue());
-                Add2DecimalNumber(worksheet, startingRow, 4, statement.GetDebitValue());
-                startingRow++;
+                writer.WriteList(index, bSheetCopy, (b, rowIndex) => new object[]
+                {
+                    rowIndex - 1,
+                    b.Name,
+                    b.GetCreditValue(),
+                    b.GetDebitValue(),
+                });
+                index = index + bSheetCopy.Count;
+                writer.Write(index, "", "Total", bSheetCopy.GetCreditTotal(), bSheetCopy.GetDebitTotal(), bSheetCopy.GetTotal());
+                
             }
         }
 
-        private static ExcelWorksheet AddWorksheet(ExcelPackage package, string sheetName)
+        private void AddCapitalAccount(CapitalAccountBook capitalAccountBook)
         {
-            return package.Workbook.Worksheets.Add(sheetName);
-        }
-
-        private void AddTableHeading(ExcelWorksheet worksheet, int startingRow, params string[] headings)
-        {
-            var index = 1;
-            foreach (var heading in headings)
+            using (ExcelWriter writer = new ExcelWriter(outputExcelFileName, "CapitalAccount"))
             {
-                AddText(worksheet, startingRow, index++, heading);
+                int index = 0;
+                object[] headings = {"S.No.", "Date", "Ledger", "Credit", "Debit", "Total"};
+                writer.Write(index++, headings);
+                writer.ApplyHeadingFormat(headings.Length);
+                writer.SetColumnsWidth(6, 12, 45, 12, 12, 12);
+
+                writer.WriteList(index, capitalAccountBook,
+                    (j, rowIndex) => new object[]
+                    {
+                        rowIndex - 1,
+                        j.Date,
+                        j.Name,
+                        j.GetCreditValue(),
+                        j.GetDebitValue(),
+                    });
+                index = index + capitalAccountBook.Count;
+                writer.Write(index, "", "Closing Balance", capitalAccountBook.GetCreditTotal(), 
+                    capitalAccountBook.GetDebitTotal(),
+                    capitalAccountBook.GetTotal());
+                
+            }
+        }
+        private void AddProfitAndLoss(ProfitAndLossBook profitAndLoss)
+        {
+            using (ExcelWriter writer = new ExcelWriter(outputExcelFileName, "P&L"))
+            {
+                int index = 0;
+                writer.Write(index++, "S.No.", "Ledger", "Earnings", "Expenditure", "Net");
+                writer.ApplyHeadingFormat(5);
+                writer.SetColumnsWidth(6, 45, 12, 12, 12);
+
+                writer.WriteList(index, profitAndLoss,
+                    (j, rowIndex) => new object[]
+                    {
+                        rowIndex - 1,
+                        j.Name,
+                        j.GetCreditValue(),
+                        j.GetDebitValue(),
+                    });
+                index = index + profitAndLoss.Count;
+                writer.Write(index,"","NetEarnings", profitAndLoss.GetCreditTotal(), profitAndLoss.GetDebitTotal(),
+                    profitAndLoss.GetTotal());
             }
         }
 
-
-        private void AddProfitAndLoss(ExcelPackage package, ProfitAndLossBook profitAndLoss)
+        private void AddTrialBalance(TrialBalanceBook trialBalance )
         {
-            ExcelWorksheet worksheet = AddWorksheet(package,"P&L");
-
-            int index = 1;
-            AddTableHeading(worksheet, index++, "Ledger", "Earnings", "Expenditure");
-            AddStatements(profitAndLoss, worksheet, index);
-            index = profitAndLoss.Count + index;
-            AddTableClosing(profitAndLoss, worksheet, index, "Net Earnings");
-            package.Save();
-        }
-
-        private void AddTableClosing(IList<Statement> statements, ExcelWorksheet worksheet, int index, string totalName)
-        {
-            AddText(worksheet, index, 1, totalName);
-            Add2DecimalNumber(worksheet, index, 2, statements.GetCreditTotal());
-            Add2DecimalNumber(worksheet, index, 3, statements.GetDebitTotal());
-            Add2DecimalNumber(worksheet, index, 4, statements.GetTotal());
-        }
-
-        private void AddStatements(IList<Statement> statements, ExcelWorksheet worksheet, int startingRow)
-        {
-            foreach (var statement in statements)
+            using (ExcelWriter writer = new ExcelWriter(outputExcelFileName, "TRB"))
             {
-                AddText(worksheet, startingRow, 1, statement.Name);
-                Add2DecimalNumber(worksheet, startingRow, 2, statement.GetCreditValue());
-                Add2DecimalNumber(worksheet, startingRow, 3, statement.GetDebitValue());
-                startingRow++;
+                int index = 0;
+                writer.Write(index++, "S.No.", "Ledger", "Credit", "Debit","Difference");
+                writer.SetColumnsWidth(6, 45, 12, 12, 12);
+                writer.ApplyHeadingFormat(5);
+                writer.WriteList(index, trialBalance,
+                    (j, rowIndex) => new object[]
+                    {
+                        rowIndex- 1,
+                        j.Name,
+                        j.GetCreditValue(),
+                        j.GetDebitValue(),
+                    });
+                index = index + trialBalance.Count;
+                writer.Write(index,"", "Total", trialBalance.GetCreditTotal(), trialBalance.GetDebitTotal(), 
+                    trialBalance.GetTotal());
             }
         }
 
-
-        private void AddTrialBalance(ExcelPackage package, TrialBalanceBook trialBalance )
+        private void AddJournal(IList<JournalStatement> journalStatements)
         {
-            ExcelWorksheet worksheet = AddWorksheet(package,"TRB");
-
-            var index = 1;
-            AddTableHeading(worksheet, index++, "Ledger", "Credit", "Debit");
-            AddStatements(trialBalance, worksheet, index);
-            index = index + trialBalance.Count;
-            AddTableClosing(trialBalance, worksheet, index,"Total");
-            package.Save();
-        }
-
-        private void AddJournal(ExcelPackage package, IList<JournalStatement> journalStatements)
-        {
-            var index = 1;
-            ExcelWorksheet worksheet = AddWorksheet(package,"Journal");
-            AddTableHeading(worksheet, index++, "S.No.", "Date", "Type", "Book", "Ledger", "Description", "Credit", "Debit");
-            
-            foreach (var statement in journalStatements)
+            using (ExcelWriter writer = new ExcelWriter(outputExcelFileName, "Journal"))
             {
-                Add0DecimalNumber(worksheet, index, 1, index - 1);
-                AddDate(worksheet, index, 2, statement.Date);
-                AddText(worksheet, index, 3, statement.EntryType);
-                AddText(worksheet, index, 4, statement.BookName.GetString());
-                AddText(worksheet, index, 5, statement.Name);
-                AddText(worksheet, index, 6, statement.AdditionalName);
-                Add2DecimalNumber(worksheet, index, 7, statement.GetCreditValue());
-                Add2DecimalNumber(worksheet, index, 8, statement.GetDebitValue());
-                index++;
+                int index = 0;
+                writer.Write(index++, "S.No.", "Date", "Type", "Book", "Ledger", "Description", "Credit", "Debit");
+                writer.SetColumnsWidth(6, 12, 4, 8, 35, 45, 12, 12);
+                writer.ApplyHeadingFormat(8);
+                writer.WriteList(index, journalStatements, 
+                    (j, rowIndex) => new object[]
+                    {
+                        rowIndex - 1,
+                        j.Date,
+                        j.EntryType,
+                        j.BookName.GetString(),
+                        j.Name,
+                        j.AdditionalName,
+                        j.GetCreditValue(),
+                        j.GetDebitValue(),
+                    });
+                
             }
-            package.Save();
         }
     }
 }
