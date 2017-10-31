6bfdcd1358b110134f733bc766f62f37e8d0f39a
Nachiappan <nachiapan@gmail.com>
2017-10-31 21:06:59 +0530

    Separated the statement separation from the container

---
+++ Simplify/Simplify/Trade/OpenPositionBook.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Simplify.Trade
+{
+    public class OpenPositionBook
+    {
+        public List<TradeStatement> OpenPositionRecords { get; set; }
+
+        public double CostOfOpenPosition { get; set; }
+
+    }
+}
--- Simplify/Simplify/Trade/ProcessedTradeStatementsContainer.cs
+++ Simplify/Simplify/Trade/ProcessedTradeStatementsContainer.cs
@@ -21,6 +21,16 @@ namespace Simplify.Trade
             InitializeEffectiveCostStatementBook();
         }
 
+        private void InitializeOpenPositionAndProfitBook(List<TradeStatement> tradeStatements)
+        {
+            var separator = new StatementsSeparator();
+            List<TradeStatement> openStatements;
+            List<SquarableStatement> closedStatements;
+            separator.SeparateStatementsAsClosedAndOpen(tradeStatements, out openStatements, out closedStatements);
+            OpenPositionBook = openStatements;
+            ProfitBook = closedStatements;
+        }
+
         private void InitializeEffectiveCostStatementBook()
         {
             EffectiveCostStatementBook = new List<CostStatement>();
@@ -59,120 +69,12 @@ namespace Simplify.Trade
             }
         }
 
-        private void InitializeOpenPositionAndProfitBook(List<TradeStatement> tradeStatements)
-        {
-            OpenPositionBook = new List<TradeStatement>();
-            ProfitBook = new List<SquarableStatement>();
-            foreach (var name in AssetNamesBook)
-            {
-                ProcessTradeStatements(tradeStatements.Where(x => x.Name == name).ToList());
-            }
-        }
-
         private void InitializeAssetNamesBook(List<TradeStatement> tradeStatements)
         {
             AssetNamesBook = tradeStatements.Select(x => x.Name).Distinct().OrderBy(x => x).ToList();
         }
+
+
         
-        private void ProcessTradeStatements(List<TradeStatement> tradeStatements)
-        {
-            var purchaseStatements = tradeStatements.Where(x => x.IsPurchase).OrderBy(x => x.Date).ToList();
-            var saleStatements = tradeStatements.Where(x => !x.IsPurchase).OrderBy(x => x.Date).ToList();
-            while (purchaseStatements.Any() && saleStatements.Any())
-            {
-                var squaredAndRemaining = SquareStatements(
-                    RemoveFirstItem(purchaseStatements),
-                    RemoveFirstItem(saleStatements));
-
-                ProfitBook.Add(squaredAndRemaining.SquarableStatement);
-                var remaining = squaredAndRemaining.RemainingTradeStatement;
-                if (remaining == null) continue;
-                if (remaining.IsPurchase)
-                    purchaseStatements.Insert(0, remaining);
-                else
-                    saleStatements.Insert(0, remaining);
-            }
-            if (purchaseStatements.Any())
-                OpenPositionBook.AddRange(purchaseStatements);
-            if (saleStatements.Any())
-                OpenPositionBook.AddRange(saleStatements);
-        }
-
-        private TradeStatement RemoveFirstItem(IList<TradeStatement> statements)
-        {
-            var statement = statements.ElementAt(0);
-            statements.RemoveAt(0);
-            return statement;
-        }
-
-        private SquarableStatementAndRemainder SquareStatements(TradeStatement ts1, TradeStatement ts2)
-        {
-            TradeStatement bigger, smaller;
-            if (ts1.Quantity > ts2.Quantity)
-            {
-                bigger = ts1;
-                smaller = ts2;
-            }
-            else
-            {
-                bigger = ts2;
-                smaller = ts1;
-            }
-
-            var brokenStatements = new BrokenTradeStatements(bigger, smaller.Quantity);
-            var squarableStatement = new SquarableStatement(new [] { brokenStatements.BrokenTradeStatement, smaller });
-            return new SquarableStatementAndRemainder(squarableStatement, brokenStatements.RemainingTradeStatement);
-
-        }
-
-        private class BrokenTradeStatements
-        {
-            public BrokenTradeStatements(TradeStatement ts, double quantity)
-            {
-                var brokenStatement = CreateACopy(ts);
-                var remainingStatement = CreateACopy(ts);
-                var initialQuantity = ts.Quantity;
-                var initialValue = ts.Value;
-                brokenStatement.Quantity = quantity;
-                remainingStatement.Quantity = initialQuantity - quantity;
-                var brokenValue = initialValue * quantity / initialQuantity;
-                brokenStatement.Value = brokenValue;
-                remainingStatement.Value = initialValue - brokenValue;
-                BrokenTradeStatement = brokenStatement;
-                if(remainingStatement.Quantity > 0.0001)
-                    RemainingTradeStatement = remainingStatement;
-            }
-
-            private static TradeStatement CreateACopy(TradeStatement ts)
-            {
-                var tradeStatement = new TradeStatement
-                {
-                    IsPurchase = ts.IsPurchase,
-                    Date = ts.Date,
-                    Name = ts.Name,
-                    TransactionTax = ts.TransactionTax,
-                    TransactionDetail = ts.TransactionDetail,
-                    Quantity = ts.Quantity,
-                    Value = ts.Value,
-                };
-                return tradeStatement;
-            }
-
-            public TradeStatement BrokenTradeStatement { get; private set; }
-            public TradeStatement RemainingTradeStatement { get; private set; }
-        }
-
-        private class SquarableStatementAndRemainder
-        {
-
-            public SquarableStatementAndRemainder(SquarableStatement squarableStatement, TradeStatement remainingTradeStatement)
-            {
-                SquarableStatement = squarableStatement;
-                RemainingTradeStatement = remainingTradeStatement;
-            }
-
-            public SquarableStatement SquarableStatement { get; set; }
-            public TradeStatement RemainingTradeStatement { get; set; }
-        }
     }
 }
