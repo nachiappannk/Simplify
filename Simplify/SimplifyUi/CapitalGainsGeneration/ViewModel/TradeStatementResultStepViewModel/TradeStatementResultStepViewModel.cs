6074a38666f355f1835210097ba9b69e21c50b72
Nachiappan <nachiapan@gmail.com>
2017-11-05 02:10:23 +0530

    Some Clean up of view models

--- Simplify/SimplifyUi/CapitalGainsGeneration/ViewModel/TradeStatementResultStepViewModel/TradeStatementResultStepViewModel.cs
+++ Simplify/SimplifyUi/CapitalGainsGeneration/ViewModel/TradeStatementResultStepViewModel/TradeStatementResultStepViewModel.cs
@@ -11,64 +11,18 @@ using SimplifyUi.Properties;
 
 namespace SimplifyUi.CapitalGainsGeneration.ViewModel.TradeStatementResultStepViewModel
 {
-    public class TradeStatementResultStepViewModel : WorkFlowStepViewModel, INotifyPropertyChanged
+    public class TradeStatementResultStepViewModel : WorkFlowStepViewModel
     {
-        public InteractionRequest<FileSaveAsNotification> FileSaveAsRequest { get; private set; }
         private ProcessedTradeStatementsContainer _statementsContainer;
 
-        
-
-        private OpenPositionViewModel _openPositionViewModel;
-        public OpenPositionViewModel OpenPositionViewModel
-        {
-            get { return _openPositionViewModel; }
-            set
-            {
-                if (_openPositionViewModel != value)
-                {
-                    _openPositionViewModel = value;
-                    FirePropertyChanged();
-                }
-            } 
-        }
-
-        private ProfitBookViewModel _profitBookViewModel;
-
-        public ProfitBookViewModel ProfitBookViewModel
-        {
-            get { return _profitBookViewModel; }
-            set
-            {
-                if (_profitBookViewModel != value)
-                {
-                    _profitBookViewModel = value;
-                    FirePropertyChanged();
-                }
-            }
-        }
-        
-        private AssetEvaluationBookViewModel _assetEvaluationBookViewModel;
-        public AssetEvaluationBookViewModel AssetEvaluationBookViewModel
-        {
-            get{ return _assetEvaluationBookViewModel;}
-            set
-            {
-                if (value != _assetEvaluationBookViewModel)
-                {
-                    _assetEvaluationBookViewModel = value;
-                    FirePropertyChanged();
-                }
-            }
-        }
-
+        public InteractionRequest<FileSaveAsNotification> FileSaveAsRequest { get; private set; }
+        public OpenPositionViewModel OpenPositionViewModel { get; set; }
+        public ProfitBookViewModel ProfitBookViewModel { get; set; }
+        public AssetEvaluationBookViewModel AssetEvaluationBookViewModel { get; set; }
         public AssetQuotesViewModel AssetQuotesViewModel { get; set; }
         public AssetEvaluationAggregatedBookViewModel AssetEvaluationAggregatedBookViewModel { get; set; }
-
         public OpenAssetSummary HoldingAssetSummary { get; set; }
-
         public ClosedAssetSummary ClosedAssetSummary { get; set; }
-
-        private NamedCommand SaveCommand;
 
         public TradeStatementResultStepViewModel()
         {
@@ -77,20 +31,24 @@ namespace SimplifyUi.CapitalGainsGeneration.ViewModel.TradeStatementResultStepVi
             CanGoToPrevious = true;
             Name = "Result";
             FileSaveAsRequest = new InteractionRequest<FileSaveAsNotification>();
-            SaveCommand = new NamedCommand("Save", new DelegateCommand(SaveOutputFile));
-            AddCommand(SaveCommand);
+            var saveCommand = new NamedCommand("Save", new DelegateCommand(SaveOutputFile));
+            AddCommand(saveCommand);
 
         }
         public void SetStatements(ProcessedTradeStatementsContainer statementsContainer)
         {
             _statementsContainer = statementsContainer;
+            AssetQuotesViewModel = new AssetQuotesViewModel(statementsContainer);
+
             OpenPositionViewModel = new OpenPositionViewModel(_statementsContainer);
+            AssetEvaluationBookViewModel = new AssetEvaluationBookViewModel(_statementsContainer.AssetEvalutionBook);
+            AssetEvaluationAggregatedBookViewModel = new AssetEvaluationAggregatedBookViewModel(_statementsContainer);
+
+            
             ProfitBookViewModel = new ProfitBookViewModel(_statementsContainer);
             HoldingAssetSummary = new OpenAssetSummary(_statementsContainer);
             ClosedAssetSummary = new ClosedAssetSummary(_statementsContainer.ClosedAssetSummaryBooks);
-            AssetEvaluationBookViewModel = new AssetEvaluationBookViewModel(_statementsContainer.AssetEvalutionBook);
-            AssetEvaluationAggregatedBookViewModel = new AssetEvaluationAggregatedBookViewModel(_statementsContainer);
-            AssetQuotesViewModel = new AssetQuotesViewModel(statementsContainer);
+            
         }
 
         
@@ -107,14 +65,6 @@ namespace SimplifyUi.CapitalGainsGeneration.ViewModel.TradeStatementResultStepVi
             var writer = new ProcessedTradeStatementsExcelGateway();
             if (File.Exists(fullPath)) File.Delete(fullPath);
             writer.Write(fullPath, _statementsContainer);
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        [NotifyPropertyChangedInvocator]
-        protected virtual void FirePropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
