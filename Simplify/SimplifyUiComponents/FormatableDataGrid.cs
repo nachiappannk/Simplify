49a57fa9dd6a3b50542b498a64070b58b9f40b04
Nachiappan <nachiapan@gmail.com>
2017-11-05 02:50:32 +0530

    Moved the custom control to another workspace

--- Simplify/SimplifyUiComponents/FormatableDataGrid.cs
+++ Simplify/SimplifyUiComponents/FormatableDataGrid.cs
@@ -8,45 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace SimplifyUi.Common
+namespace SimplifyUiComponents
 {
-    /// <summary>
-    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
-    ///
-    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:SimplifyUi.Common"
-    ///
-    ///
-    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:SimplifyUi.Common;assembly=SimplifyUi.Common"
-    ///
-    /// You will also need to add a project reference from the project where the XAML file lives
-    /// to this project and Rebuild to avoid compilation errors:
-    ///
-    ///     Right click on the target project in the Solution Explorer and
-    ///     "Add Reference"->"Projects"->[Browse to and select this project]
-    ///
-    ///
-    /// Step 2)
-    /// Go ahead and use your control in the XAML file.
-    ///
-    ///     <MyNamespace:FormatableDataGrid/>
-    ///
-    /// </summary>
     public class FormatableDataGrid : DataGrid
     {
         public FormatableDataGrid() : base()
@@ -64,11 +29,11 @@ namespace SimplifyUi.Common
             var displayFormat = GetColumnFormat(e.PropertyDescriptor);
             if (!string.IsNullOrEmpty(displayFormat))
             {
-                ((DataGridTextColumn) e.Column).Binding.StringFormat = displayFormat;
+                ((DataGridTextColumn)e.Column).Binding.StringFormat = displayFormat;
             }
             var isEditable = IsColumnEditable(e.PropertyDescriptor);
             e.Column.IsReadOnly = !isEditable;
-            if(isEditable)
+            if (isEditable)
             {
                 e.Column.Header = e.Column.Header + Environment.NewLine + "(Editable)";
                 e.Column.CellStyle = new Style(typeof(DataGridCell));
